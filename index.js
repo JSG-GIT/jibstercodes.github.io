b4a5e09499cf1b26de1b8b92ff776841a222777d
JibsterCodes <j.github@rutger.nl>
2022-06-26 21:57:26 +0200

    Troll added

--- index.js
+++ index.js
@@ -1,33 +1,40 @@
-var ParticleAmount = 0;
-
+var ParticleAmount = 0.00;
+var fps = 60;
+var show = 0;
 //Function definitions
 
 $(document).ready(function()
 {
-    setInterval(TickFunction, 20);
+    setInterval(TickFunction, 1000/fps);
+    $("#video").hide();
 });
 
 function TickFunction()
 {
     //Update math
-    ParticleAmount = ParticleAmount + 1;
+    ParticleAmount += 1/fps;
 
     //Update visuals
-    UpdateVarDisplay(ParticleAmount, true);
+    NewText = ToText(ParticleAmount)
+    UpdateVarDisplay("ParticleAmount", NewText);
+
+    if(ParticleAmount > 10 && show == 0){show = 1; $("#video").show(); $("#video").play()}
 }
 
-function UpdateVarDisplay(variable, IsNumber)
+function ToText(variable)
 {
-    if(IsNumber)
+    if(typeof variable === "number")
     {
+        if(variable < 10000){return variable.toFixed(2).toString();}
         var exponent = Math.floor(Math.log10(variable));
-        var value = parseFloat(Math.pow(variable, -exponent)).toFixed(2);
-        var text = value + "e" + exponent;
+        var value = (Math.pow(10, -exponent) * variable).toFixed(2);
+        return text = value + "e" + exponent;
     }
-    else
-    {
-        var text = variable.toString();
-    }
+    return variable.toString();
+}
 
-    $("#" + variable.toString()).text(text);
+function UpdateVarDisplay(id, text)
+{
+    $("#" + id).text(text);
+    console.log("#" + id)
 }

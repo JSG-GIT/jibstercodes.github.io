21514b6b371af84106d86f8edeae42e376320631
JibsterCodes <j.github@rutger.nl>
2022-06-27 17:39:23 +0200

    version: 0.0.2-beta

--- index.js
+++ index.js
@@ -8,6 +8,13 @@ var Producer2_N = 0;
 var Producer2_B = 0;
 var Producer2_P = 0;
 
+var Producer3_N = 0;
+var Producer3_B = 0;
+var Producer3_P = 0;
+
+var upg1 = false;
+var upg2 = false;
+
 var lastClick;
 var focused = true;
 
@@ -15,7 +22,33 @@ var focused = true;
 $(document).ready(function() {
     window.requestAnimationFrame(ActiveTickFunction);
     Buttons();
+    $("#prodrow3").hide();
 });
+
+
+//MATH
+function UpdateMath(dt)
+{
+    //Update math
+    if(upg1){
+        Producer1_P = Producer1_N*Math.pow(1.15, Producer1_B);
+        Producer2_P = Producer2_N*Math.pow(1.2, Producer2_B);
+        Producer3_P = Producer3_N*Math.pow(1.25, Producer3_B);
+    }
+    else{
+        Producer1_P = Producer1_N*Math.pow(Producer1_B, 0.15);
+        Producer2_P = Producer2_N*Math.pow(Producer2_B, 0.2);
+        Producer3_P = Producer3_N*Math.pow(Producer3_B, 0.25);
+    }
+    if(upg2){
+        Producer1_P = Producer1_P*(1+Math.log10(Producer1_N+1));
+        Producer2_P = Producer2_P*(1+Math.log10(Producer2_N+1));
+        Producer3_P = Producer3_P*(1+Math.log10(Producer3_N+1));
+    }
+    Particle_N += Producer1_P*dt;
+    Producer1_N += Producer2_P*dt;
+    Producer2_N += Producer3_P*dt;
+}
 
 function Buttons(){
     $(".prodbtn").click(function(){
@@ -27,19 +60,11 @@ function Buttons(){
             window["Producer" + (producer) + "_N"] += 1;
         }
     });
+    $(".upgbtn").click(function(){
+        if((this.id==="1") && (Particle_N >= 500)){Particle_N-=500; upg1 = true;}
+        if((this.id==="2") && (Particle_N >= Math.pow(10,6))){Particle_N-=Math.pow(10,6); upg2 = true;}
+    });
 }
-
-//MATH
-function UpdateMath(dt)
-{
-    //Update math
-    Producer1_P = Producer1_N*Math.pow(Producer1_N, 0.05);
-    Particle_N += Producer1_P*dt;
-
-    Producer2_P = Producer2_N;
-    Producer1_N += Producer2_P*dt;
-}
-
 
 //Updating the screen
 function UpdateHTML(id, text)
@@ -88,9 +113,9 @@ function ToText(variable)
 function UpdateDisplay(){
     NewText = ToText(Particle_N);
     UpdateHTML("Particle_N", NewText);
-
-    for(i=1;i<3;i++){
-        text = "Producer[" + i + "]: " + ToText(window["Producer" + i + "_N"]) + " (" + ToText(window["Producer" + i + "_B"]) + ")";
+    if(Particle_N > Math.pow(10,5)){$("#prodrow3").show();}
+    for(i=1;i<4;i++){
+        text = "Producer[" + i + "]: " + ToText(window["Producer" + i + "_N"]) + " (" + Math.floor(window["Producer" + i + "_B"]) + ")";
         id = "Prod" + i + "_N";
         UpdateHTML(id, text);
         if(i === 1){
@@ -111,5 +136,5 @@ function UpdateDisplay(){
 }
 
 function getProducerCost(index){
-    return Math.ceil(Math.pow(Math.pow(10, Math.pow(index, Math.pow(10, 1/10))), (window["Producer" + index + "_B"])/(5-index) + 1));
+    return Math.ceil(Math.pow(Math.pow(10, Math.pow(index, Math.pow(10, index/10))), (window["Producer" + index + "_B"])/(5-index) + 1));
 }
